85c17ba87dc02d6682fd0c24c93646490b46faa8
Adarsh R N <Adarsh R N@TIGER>
2020-07-31 15:15:21 +0530

    Second commit

--- Project_Maven/src/test/java/Test/Ae1_Demo.java
+++ Project_Maven/src/test/java/Test/Ae1_Demo.java
@@ -1,11 +1,12 @@
 package Test;
 
 import org.testng.Reporter;
+import org.testng.annotations.Test;
 
 public class Ae1_Demo {
-	
+	@Test
 	public void testA()
 	{
-		Reporter.log("Hiii",true);
+		Reporter.log("Hiii Hellooo",true);
 	}
 }
